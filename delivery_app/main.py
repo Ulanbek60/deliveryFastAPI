941676c34513716397fe8d5981048ddf41751eb8
Ulanbek60 <uislambekuulu@gmail.com>
2025-11-22 13:31:52 +0600

    docker compose

--- delivery_app/main.py
+++ delivery_app/main.py
@@ -13,23 +13,31 @@ from delivery_app.api.endpionts import auth, category, contact, store, product
 from starlette.middleware.sessions import SessionMiddleware
 from delivery_app.config import SECRET_KEY
 
+
 async def init_redis():
-    return redis.Redis.from_url('redis://localhost', encoding='utf-8', decode_responses=True)
+    return redis.Redis.from_url(
+        'redis://redis:6379',
+        encoding='utf-8',
+        decode_responses=True
+    )
+
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    redis = await init_redis()
-    await FastAPILimiter.init(redis)
+    redis_conn = await init_redis()
+    await FastAPILimiter.init(redis_conn)
     yield
-    await redis.close()
+    await redis_conn.close()
+
 
 delivery = fastapi.FastAPI(title='Delivery', lifespan=lifespan)
-delivery.add_middleware(SessionMiddleware, secret_key="SECRET_KEY")
+delivery.add_middleware(SessionMiddleware, secret_key=SECRET_KEY)
 
 admin = Admin(delivery, engine)
 
 oauth2_schema = OAuth2PasswordBearer(tokenUrl='/auth/login')
 password_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
+
 setup_admin(delivery)
 
 delivery.include_router(auth.auth_router)
@@ -38,6 +46,6 @@ delivery.include_router(store.store_router)
 delivery.include_router(contact.contact_router)
 delivery.include_router(product.product_router)
 
+
 if __name__ == "__main__":
     uvicorn.run(delivery, host="127.0.0.1", port=8001)
-
